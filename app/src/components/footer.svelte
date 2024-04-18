96256fb7ffaf16263dbb280328f40b2a28292ede
Gabriel Tower <gabriel.tower@baxter-academy.org>
2024-04-17 22:30:57 -0400

    Fixed home page formatting
    Headers and text had no formatting at all. So I added some.

--- app/src/components/footer.svelte
+++ app/src/components/footer.svelte
@@ -4,13 +4,13 @@
 
 <div class="mt-20 w-100% h-200" style:background>
 
-    <h1 class="flex justify-center text-stone-500 text-4xl">Contact Us</h1>
-    
-    <p class="flex justify-center text-stone-500 text-xl mt-2">gmtower1@gmail.com</p>
-    <p class="flex justify-center text-stone-500 text-xl mb-2">+1(207)-608-6888</p>
+    <h1 class="flex justify-center text-zinc-950 text-4xl">Contact Us</h1>
 
-    <p class="flex justify-center text-stone-500 text-xl mt-2">jenny@gmail.com</p>
-    <p class="flex justify-center text-stone-500 text-xl mb-2">+1(207)-867-5309</p>
+    <p class="flex justify-center text-zinc-950 text-xl mt-2">heather@tp4s.com</p>
+    <p class="flex justify-center text-zinc-950 text-xl mb-2">+1(207)-867-5309</p>
+
+    <p class="flex justify-center text-zinc-950 text-xl mt-2">gmtower1@gmail.com</p>
+    <p class="flex justify-center text-zinc-950 text-xl mb-2">+1(207)-607-6888</p>
 
     <slot />
 </div>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -13,17 +13,17 @@
     <Hero title="Welcome to the Bank of Mommy"/>
 
     <div class="bg-sky-50">
-        <h1>Our Misson</h1>
-        <p>We aim to give families a fun and simple way to track their kids' money, to teach kids the basics of banking, and to encourage them to develop healthy financial habits.</p>
-        <h1>Our Services</h1>
-        <p>We are not an actual bank - you keep your kids' money, and we just help you keep track of each account.  With our help, you can be a safe place to store your kids money, give them allowance and interest, buy any gift cards from them, help them make purchases with their own money, and show them their current balance at any time.</p>
+        <h1 class="flex justify-center text-4xl m-4">Our Misson</h1>
+        <p class="flex justify-center text-xl m-4">We aim to give families a fun and simple way to track their kids' money, to teach kids the basics of banking, and to encourage them to develop healthy financial habits.</p>
+        <h1 class="flex justify-center text-4xl m-4">Our Services</h1>
+        <p class="flex justify-center text-xl m-4">We are not an actual bank - you keep your kids' money, and we just help you keep track of each account.  With our help, you can be a safe place to store your kids money, give them allowance and interest, buy any gift cards from them, help them make purchases with their own money, and show them their current balance at any time.</p>
 
         <br>
 
-        <h1>How The Bank Of Mommy Works</h1>
+        <h1 class="flex justify-center text-4xl m-4">How The Bank Of Mommy Works</h1>
         
         <div class="flex justify-center">
-            <ul class="max-w-md space-y-1 text-stone-500 list-disc list-inside dark:text-gray-400">
+            <ul class="max-w-md space-y-1 text-zinc-950 list-disc list-inside dark:text-zinc-950">
                 <li>Your kids give you all their cash</li>
                 <li>The Bank of Mommy will keep track of how much money each kid has</li>
                 <li>You determine weather you want to give your kids allowance, birthday, and other gifts, and a monthly interest payment on their savings. Bank of Mommy will keep track of that too.</li>
