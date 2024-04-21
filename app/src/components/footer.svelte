1db9513e50603aac7c070a50963b86ef72011184
Gabriel Tower <gabriel.tower@baxter-academy.org>
2024-04-21 13:22:42 -0400

    Moving it over to a grid layout

--- app/src/components/footer.svelte
+++ app/src/components/footer.svelte
@@ -4,22 +4,34 @@
 
 <div class="mt-20 w-100% h-200" style:background>
 
-    <h1 class="flex justify-center text-zinc-950 text-4xl">Contact Us</h1>
-    <hr class="h-px m-2 bg-gray-200 border-0 dark:bg-gray-700">
-
-    <div class="flex justify-center flex-row">
+    <div class="flex justify-right flex-row ml-10 mr-10">
         <div>
-            <p class="flex justify-center text-zinc-950 text-xl mt-2">Gabriel Tower</p>
-            <p class="flex justify-center text-zinc-950 text-xl">gmtower1@gmail.com</p>
-            <p class="flex justify-center text-zinc-950 text-xl mb-2">+1(207)-607-6888</p>
+            <h1 class="flex justify-right text-zinc-950 text-xl ml-10">Contact Us</h1>
+            <hr class="h-px w-40 ml-10 bg-gray-200 border-0 dark:bg-gray-700">
+            <div class="grid grid-cols-3 gap-4">
+                <div>
+                    <p class="flex justify-center text-zinc-950 text-m mt-2">Gabriel Tower</p>
+                    <p class="flex justify-center text-zinc-950 text-s">gmtower1@gmail.com</p>
+                    <p class="flex justify-center text-zinc-950 text-s mb-2">+1(207)-607-6888</p>
+                </div>
+        
+                <div>
+                    <p class="flex justify-center text-zinc-950 text-m mt-2">Heather Krey</p>
+                    <p class="flex justify-center text-zinc-950 text-s">heather@tp4s.com</p>
+                    <p class="flex justify-center text-zinc-950 text-s mb-2">+1(484)-433-0993</p>
+                </div>
+            </div>
         </div>
 
-        <div class="m-4"/>
-
         <div>
-            <p class="flex justify-center text-zinc-950 text-xl mt-2">Heather Krey</p>
-            <p class="flex justify-center text-zinc-950 text-xl">heather@tp4s.com</p>
-            <p class="flex justify-center text-zinc-950 text-xl mb-2">+1(484)-433-0993</p>
+            <h1 class="flex justify-right text-zinc-950 text-xl ml-10">Relevent Links</h1>
+            <hr class="h-px w-40 ml-10 bg-gray-200 border-0 dark:bg-gray-700">
+
+            <ul>
+                <li>
+                    <a href="https://worldclasstutoring.com/">World Class Tutoring</a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
